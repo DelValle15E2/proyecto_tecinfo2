9c82bdde83ac318b0fb4a8aa2760854ccfcd102e
DelValle15E2 <A100239017@my.uvm.edu.mx>
2025-04-30 13:15:52 -0600

    mi_script.js

---
+++ mi_script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const boton = document.getElementById("miBoton");
+
+    boton.addEventListener("click", function() {
+        alert("¡Gracias por visitar nuestro proyecto social!");
+    });
+});
